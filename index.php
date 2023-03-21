4fe08bda19ed873a369c5e750d000c837cf485f8
marco <marcoavegagon@gmail.com>
2023-03-20 20:45:54 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once './controllers/Autoload.php';
+$autoload = new Autoload();
+
+$route = isset($_GET['r']) ? $_GET['r'] : 'home';
+
+$iglesia2 = new Router($route);
+
+/*
+
+*/
---
+++ views/home.php
@@ -0,0 +1,13 @@
+<?php
+$template = '
+
+<div class="home container">
+  <h2>Pagina Principal.</h2>           
+ 
+</div>
+
+';
+
+printf(
+  $template,
+);
